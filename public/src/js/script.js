bfffba413d77fe7c08c47bb2029f34ff39fbd397
Luke Wood <luke.wood@outlook.com>
2020-09-14 19:46:09 +1000

    Fixed stupid RAF issues...

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,26 +1,38 @@
 (function () {
-  function animator(handler) {
-    function (config) {
-      return function updater() {
+  let needForRAF = true;
+  let scale;
+  const screen = document.querySelector('.screen');
+function animator(handler) {
+    return function updater(config) {
+      if (updateRequired) {
+        updateRequired = false;
         handler(config);
         requestAnimationFrame(updater);
       }
     }
   }
 
-  function draw(e) {
-    const screen = document.querySelector('.screen');
+  function mouseMove(e) {
+    e.preventDefault();
     const dim = document.body.getBoundingClientRect();
     const { clientX: x, clientY: y } = e;
     const posX = x - (dim.width / 2);
     const posY = y - (dim.height / 2);
     const distance = Math.hypot(posX, posY);
     const maxdistance = Math.hypot(dim.width, dim.height);
-    const scale = distance / maxdistance;
+    scale = distance / maxdistance;
+    if (needForRAF) {
+      needForRAF = false;            // no need to call rAF up until next frame
+      requestAnimationFrame(update); // request 60fps animation
+    };
+  }
+
+  function update() {
+    needForRAF = true; // rAF now consumes the movement instruction so a new one can come
     screen.style.background = `radial-gradient(circle farthest-side, transparent, rgba(0, 0, 0, ${scale * 0.9}))`;
   }
 
-  const animateDraw = animator(draw);
 
-  document.body.addEventListener('mousemove', (e) => animateDraw(e));
+  document.body.addEventListener('mousemove', mouseMove);
+
 }());
