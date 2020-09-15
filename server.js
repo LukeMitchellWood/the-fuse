005185ffc34f57307df0f5a4851af673cfce6d3d
Luke Wood <luke.wood@outlook.com>
2020-09-15 14:41:26 +1000

    Updated security of express app. Implemented security best practices.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.port || 3000;
+
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
+
+app.use(helmet());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.json({ message: 'up and running...' });
+app.get('/api', (req, res) => {
+  res.json({ message: 'api entry point...' });
 });
