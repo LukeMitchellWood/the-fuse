ebb8b04a544081b10ae49bb1f66d4466a8a590ef
Luke Wood <luke.wood@outlook.com>
2020-09-15 10:53:00 +1000

    Updated to contain meta data. JSON-LD, general, facebook, and twitter.

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,37 +1,27 @@
-(function () {
-  let needForRAF = true;
-  let scale;
-  const screen = document.querySelector('.screen');
-function animator(handler) {
-    return function updater(config) {
-      if (updateRequired) {
-        updateRequired = false;
-        handler(config);
-        requestAnimationFrame(updater);
-      }
+/* eslint no-param-reassign: ["error", { "props": false }] */
+/* eslint max-classes-per-file: ["error", 2] */
+
+(function BackgroundModule() {
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+
+  class Grid {
+    constructor(rows, color) {
+      this.rows = rows;
+      this.color = color;
     }
   }
+  class Square {
+    constructor(x, y, color, size) {
+      this.x = x;
+      this.y = y;
+      this.color = color;
+      this.size = size;
+    }
 
-  function mouseMove(e) {
-    e.preventDefault();
-    const dim = document.body.getBoundingClientRect();
-    const { clientX: x, clientY: y } = e;
-    const posX = x - (dim.width / 2);
-    const posY = y - (dim.height / 2);
-    const distance = Math.hypot(posX, posY);
-    const maxdistance = Math.hypot(dim.width, dim.height);
-    scale = distance / maxdistance;
-    if (needForRAF) {
-      needForRAF = false;            // no need to call rAF up until next frame
-      requestAnimationFrame(update); // request 60fps animation
-    };
+    draw(c) {
+      c.fillStyle = this.color;
+      c.fillRect(this.x, this.y, this.size, this.size);
+    }
   }
-
-  function update() {
-    needForRAF = true; // rAF now consumes the movement instruction so a new one can come
-    screen.style.background = `radial-gradient(circle farthest-side, transparent, rgba(0, 0, 0, ${scale * 0.9}))`;
-  }
-
-  document.body.addEventListener('mousemove', mouseMove);
-
 }());
